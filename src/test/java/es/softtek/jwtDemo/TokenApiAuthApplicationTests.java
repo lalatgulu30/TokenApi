6ece11ab67cbe8e25b1b3ef981a537948e77e462
lalatgulu30 <lalatendukeshariprusty@gmail.com>
2020-09-16 13:43:15 +0530

    Token API Auth Demo spring boot

---
+++ src/test/java/es/softtek/jwtDemo/TokenApiAuthApplicationTests.java
@@ -0,0 +1,15 @@
+package es.softtek.jwtDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TokenApiAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
